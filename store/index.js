c7a55dd41cfec2b0d93f3fb995aa6c167b05a56c
stefanbemelmans <stefanbemelmans898@gmail.com>
2018-04-10 01:26:59 -0500

    finally fixed, starting TX viewer

--- store/index.js
+++ store/index.js
@@ -6,17 +6,18 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 // import * as firebase from 'firebase';
 //REDUX devtools integration
 
- 
+// composeWithDevTools()
 // const store = createStore(reducer, composeWithDevTools(
 //   applyMiddleware(...middleware),
-  // other store enhancers if any 
+// other store enhancers if any 
 // ));
-const store = createStore(reducers,
-   
-       composeWithDevTools(applyMiddleware(thunkMiddleware))
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    
-    
+const store = createStore(
+  reducers,
+
+  composeWithDevTools(applyMiddleware(thunkMiddleware)),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+
 );
 
 export default store;
